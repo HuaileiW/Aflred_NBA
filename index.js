94c173060a0ef386932ff7873faa1c49299d61ef
Huailei Wang <Huailei.wang@icloud.com>
2019-01-15 10:10:47 +0800

    add workflow for LoL

--- index.js
+++ index.js
@@ -4,83 +4,51 @@ const flow = require('lodash').flow;
 
 const startTime = moment().format('YYYY-MM-DD');
 const endTime = moment().add('3', 'day').format('YYYY-MM-DD');
+const host = 'https://apps.game.qq.com/lol/match/apis/searchBMatchInfo_bak.php?p8=5&p1=115&p6=3&page=1&pagesize=10&_=1547452018654'
 
-alfy.fetch(`https://matchweb.sports.qq.com/kbs/list?from=NBA_PC&columnId=100000&startTime=${startTime}&endTime=${endTime}`, {
-  transform: ({ data }) => ({ ...data }),
+alfy.fetch(`${host}&p9=${startTime}%2000:00:00&p10=${endTime}%2023:59:59`, {
+  transform: ({ msg: { result } }) => {
+    return result;
+  },
 })
   .then((games) => {
     const items = dataFlow(games);
-
     alfy.output(items);
   })
   .catch(error => {
     alfy.error(`出错了...  ${error}`)
   });
 
-const isTodaysGame = gameTime => moment().add(1, 'day').startOf('day') > moment(gameTime);
-const isBeforeStart = matchPeriod => matchPeriod === '0';
-const isGameLive = matchPeriod => matchPeriod === '1';
-const gameStatus = matchPeriod => isBeforeStart(matchPeriod) ?
-  '未开始' :
-  isGameLive(matchPeriod) ?
-  '直播中' :
-  '已结束';
-
-const getTitle = ({ rightName, leftName, leftGoal, rightGoal, matchPeriod, startTime: gameTime }) => {
-  const beforeGame = isBeforeStart(matchPeriod);
-  const isLive = isGameLive(matchPeriod);
-  const isInToday = isTodaysGame(gameTime);
-
-  const title = beforeGame ?
-    `${leftName} @ ${rightName}` :
-    `${leftName}(${leftGoal}) @ ${rightName}(${rightGoal})`;
-
-  return isInToday ? `${title} ${gameStatus(matchPeriod)}` : title;
+const statusList = {
+  1: '未开始',
+  2: '进行中',
+  3: '已结束',
+  4: ''
 }
 
-const getSubTitle = ({ startTime: gameTime, matchPeriod, quarter, quarterTime }) => {
-  const beforeGame = isBeforeStart(matchPeriod);
-  const isInToday = isTodaysGame(gameTime);
-
-  return !isInToday ?
-    moment(gameTime).format('MM-DD HH:mm') :
-    beforeGame ?
-      `Today... ${moment(gameTime).format('HH:mm')} 距离开始还有${moment(gameTime).diff(moment(), 'hours')}小时` :
-      `Today... ${quarter} ${quarterTime}`;
-}
-
-const getIcon = ({ isPay, rightId, leftId }) => {
-  if (rightId === '13' || leftId === '13') {
-    return './laker.png';
-  } else if (rightId === '10' || leftId === '10') {
-    return './rocket.png';
+const getTitle = ({ bMatchName, MatchStatus = 4, ScoreA, ScoreB }) => {
+  const status = statusList[MatchStatus];
+  let matchName = bMatchName
+  if (MatchStatus === 2) {
+    const teams = bMatchName.split('vs');
+    matchName = `${teams[0]}(${ScoreA}) vs ${teams[1]}(${ScoreB})`;
   }
-  return isPay === '1' ? './vip.png' : './free.png'
+
+  return `${matchName} - ${status}`;
 }
 
-const mapData = (games) => {
-  let items = [];
-  for (let date in games) {
-    games[date].forEach(game => {
-      const { webUrl, matchPeriod, isPay } = game;
-      const beforeGame = isBeforeStart(matchPeriod);
-      const title = getTitle(game)
-      const subtitle = getSubTitle(game);
-
-      const item = {
-        title,
-        subtitle,
-        arg: webUrl,
-        icon: {
-          path: getIcon(game),
-        }
-      }
-      items.push(item);
-    })
-  }
-  return items;
+const getSubtitle = ({ MatchDate }) => {
+  return MatchDate
 }
 
+const mapData = games => {
+  return games.map((game) => {
+    return {
+      title: getTitle(game),
+      subtitle: getSubtitle(game),
+    }
+  })
+}
 
 const dataFlow = flow([
   mapData,
